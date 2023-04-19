832f5b04110c54ec07838d81ea9bb1b724aa8dd7
Asror <Asrorgmail.com>
2023-04-19 10:50:38 +0500

    Footer qilindi

--- js/allproducts.js
+++ js/allproducts.js
@@ -8,18 +8,22 @@ images.forEach((e) => {
 })
 
 const min = document.querySelector('.minus')
+const sum = document.querySelector('.sum span')
 const plus = document.querySelector('.plus')
 let indicator = document.querySelector('.amount__indicator')
 let num = +indicator.innerHTML
-
+let count = sum.innerHTML
 plus.addEventListener('click', () => {
     if (indicator.innerHTML < 10) {
         indicator.innerHTML++
+        sum.innerHTML = count * indicator.innerHTML
     }
 })
 
 min.addEventListener('click', () => {
     if (indicator.innerHTML > 0) {
         indicator.innerHTML-- 
+        sum.innerHTML = count * indicator.innerHTML
     }
 })
+
